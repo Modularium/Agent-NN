9ffd5aac999bb7ba109c41d3defbd709dfdf5673
sam <sschimmelpfennig@proton.me>
2025-07-02 13:07:51 +0200

    Improve Flowise component

--- integrations/flowise-agentnn/AgentNN.ts
+++ integrations/flowise-agentnn/AgentNN.ts
@@ -1,13 +1,25 @@
 import axios from 'axios';
 
 export default class AgentNN {
-  constructor(private endpoint: string) {}
+  constructor(
+    private endpoint: string,
+    private taskType = 'chat',
+    private headers: Record<string, string> = {},
+    private timeout = 10000
+  ) {}
 
-  async run(question: string): Promise<string> {
-    const { data } = await axios.post(`${this.endpoint}/task`, {
-      task_type: 'chat',
-      input: question,
-    });
-    return data.result as string;
+  async run(payload: unknown): Promise<any> {
+    const { data } = await axios.post(
+      `${this.endpoint}/task`,
+      {
+        task_type: this.taskType,
+        input: payload,
+      },
+      {
+        headers: this.headers,
+        timeout: this.timeout,
+      }
+    );
+    return data.result ?? data;
   }
 }

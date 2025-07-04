c242ced8fa16fa8ab2096182dd8f319a708e77a5
sam <sschimmelpfennig@proton.me>
2025-07-04 16:42:09 +0200

    Create index.ts

---
+++ frontend/agent-ui/src/components/ui/index.ts
@@ -0,0 +1,8 @@
+// src/components/ui/index.ts
+export { Button } from './Button'
+export { Card } from './Card'
+export { Input } from './Input'
+export { Badge } from './Badge'
+export { LoadingSpinner } from './LoadingSpinner'
+export { Modal } from './Modal'
+export { ProgressBar } from './ProgressBar'

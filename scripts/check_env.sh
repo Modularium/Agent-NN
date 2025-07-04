7da5e8d30925ec269448024d851f58df2a276365
sam <sschimmelpfennig@proton.me>
2025-07-04 13:51:02 +0200

    Update check_env.sh

--- scripts/check_env.sh
+++ scripts/check_env.sh
@@ -4,4 +4,5 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$SCRIPT_DIR/helpers/env.sh"
 
-check_env
+# Run environment check using the check_environment function
+check_environment
